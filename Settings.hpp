5627f004ee39a3f01195b42e477190979dabaea4
Keksovmen <Keksovmen@yandex.ru>
2022-01-02 21:38:40 +0300

    Made alarm time settings for further connecting to EEPROM

--- Settings.hpp
+++ Settings.hpp
@@ -2,6 +2,9 @@
 #define SETTINGS_H
 
 #include <stdint.h>
+#include "Clock.hpp"
+
+#define ALARM_VECTOR_SIZE 2
 
 //in seconds
 extern uint8_t BACKLIGHT_DURATION;
@@ -12,9 +15,11 @@ extern uint8_t ALARM_AUTO_OFF_DURATION;
 //setting functionality
 //must call first, will init from EEPROM
 void initSettingsParameters();
+
+
 void setBackLightDuration(uint8_t durationS);
 void setAlarmAutoOffAfter(uint8_t seconds);
-
-
+void setAlarmTime(const Time& time, uint8_t alarmId);
+const Time& getAlarmTime(uint8_t alarmId);
 
 #endif
