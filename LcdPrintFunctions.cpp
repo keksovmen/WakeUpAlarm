4d2c0eb8e6b5f914412d7431f1fe4c8dffa241c7
Keksovmen <Keksovmen@yandex.ru>
2022-01-02 16:13:19 +0300

    Made use of printZeroPaddedInt in date time printing

--- LcdPrintFunctions.cpp
+++ LcdPrintFunctions.cpp
@@ -1,51 +1,27 @@
 #include "LcdPrintFunctions.hpp"
 #include "ProgrammState.hpp"
 #include "Util.hpp"
-// extern LiquidCrystal_I2C lcd;
 
 char intToAscii(int8_t digit){
 	return char(48 + digit);
 }
 
-void convertIntToZeroPaded(int8_t val, char buffer[2]){
-	uint8_t tens = val / 10;
-	uint8_t ones = val - tens * 10;
-	buffer[0] = intToAscii(tens);
-	buffer[1] = intToAscii(ones);	
-}
-
-
-
-
 void displayTime(const Time& time, bool enableSecunds){
-	char buffer[3] = {'\0', '\0', '\0'};
-	
-	convertIntToZeroPaded(time.hours, buffer);
-	lcd.print(buffer);
+	printZeroPaddedInt(time.hours, 2);
 	lcd.print(':');
-	
-	convertIntToZeroPaded(time.minutes, buffer);
-	lcd.print(buffer);
-	
+	printZeroPaddedInt(time.minutes, 2);
 	if (enableSecunds){
 		lcd.print(':');
-		convertIntToZeroPaded(time.seconds, buffer);
-		lcd.print(buffer);
+		printZeroPaddedInt(time.seconds, 2);
 	}
 }
 
 void displayDate(const Date& date){
-	char buffer[3] = {'\0', '\0', '\0'};
-	
-	convertIntToZeroPaded(date.day, buffer);
-	lcd.print(buffer);
+	printZeroPaddedInt(date.day, 2);
 	lcd.print('.');
-	
-	convertIntToZeroPaded(date.month, buffer);
-	lcd.print(buffer);
+	printZeroPaddedInt(date.day, 2);
 	lcd.print('.');
-	
-	lcd.print(date.year);
+	printZeroPaddedInt(date.year, 4);
 }
 
 void displayDateTime(const Clock& clk){
