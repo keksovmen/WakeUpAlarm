1323df1cf846a9e9f1375bf0a1138b0d3375cf3b
Keksovmen <Keksovmen@yandex.ru>
2022-01-05 15:26:38 +0300

    Made TemperatureSensor handler

---
+++ TemperatureSensor.cpp
@@ -0,0 +1,43 @@
+#include "TemperatureSensor.hpp"
+
+
+void TemperatureSensor::consumeTime(int32_t deltaTime){
+	timer.consumeTime(deltaTime);
+	if (timer.eventReady()){
+		timer.consumeEvent();
+		if (isAsked){
+			//read result
+			isAsked = false;
+			setTemp();
+		}else{
+			//ask result
+			isAsked = true;
+			askTemp();
+		}
+	}
+}
+
+
+void TemperatureSensor::init(){
+	sensor.begin();
+	sensor.setResolution(13);
+	sensor.setWaitForConversion(false);
+	//read from EEPROM
+	askPeriod = 30;
+	timer.startTimer(0);
+}
+
+void TemperatureSensor::setTemperaturePeriod(uint8_t period){
+	//write to EEPROM
+	askPeriod = period;
+}
+
+void TemperatureSensor::askTemp(){
+	sensor.requestTemperatures();
+	timer.startTimer(1);
+}
+
+void TemperatureSensor::setTemp(){
+	temperature = sensor.getTempCByIndex(0);
+	timer.startTimer(askPeriod);
+}
---
+++ TemperatureSensor.hpp
@@ -0,0 +1,51 @@
+#ifndef TEMPERATURE_SENSOR
+#define TEMPERATURE_SENSOR
+
+#include <stdint.h>
+#include <OneWire.h>
+
+#define REQUIRESALARMS false
+#include <DallasTemperature.h>
+#include "TimeConsumer.hpp"
+#include "Timer.hpp"
+
+#define MIN_TEMPERATURE_PERIOD 1
+#define MAX_TEMPERATURE_PERIOD 60
+
+
+class TemperatureSensor : public TimeConsumer
+{
+	public:
+		TemperatureSensor(int sensorPin){
+			oneWire = OneWire(sensorPin);
+			sensor = DallasTemperature(&oneWire);
+		}
+		
+		void consumeTime(int32_t deltaTime) override;
+		void init();
+		
+		float getTemperature()const{return temperature;}
+		
+		void setTemperaturePeriod(uint8_t period);
+		const uint8_t& getTemperaturePeriod() const {return askPeriod;}
+		
+		
+	private:
+		OneWire oneWire;
+		DallasTemperature sensor;
+		Timer timer;
+	
+		float temperature = DEVICE_DISCONNECTED_C;
+		uint8_t askPeriod;
+		bool isAsked = false;
+		
+		
+		void askTemp();
+		void setTemp();
+		
+		
+		
+};
+
+
+#endif
